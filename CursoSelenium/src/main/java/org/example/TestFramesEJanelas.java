9af608d7e3510d5708fa96c397593b96ca2ceba3
Pedro Leão <63025279+PedroLeao85@users.noreply.github.com>
2023-09-13 18:29:40 -0300

    Adiciona método deveInteragirComJanelas

--- CursoSelenium/src/main/java/org/example/TestFramesEJanelas.java
+++ CursoSelenium/src/main/java/org/example/TestFramesEJanelas.java
@@ -9,10 +9,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class TestFrame {
+public class TestFramesEJanelas {
 
     @Test
-    public void TesteFrames(){
+    public void deveInteragirComFrames(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pedro\\Documents\\Selenium\\VersaoSelenium\\116\\chromedriver-win64\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
@@ -31,9 +31,22 @@ public class TestFrame {
         driver.findElement(By.id("elementosForm:nome")).sendKeys(msg);
 
 
+    }
+    @Test
+    public void deveInteragirComJanelas() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pedro\\Documents\\Selenium\\VersaoSelenium\\116\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 
 
-
+        driver.findElement(By.id("buttonPopUpEasy")).click();
+        driver.switchTo().window("Popup");
+        driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+        driver.close();
+        driver.switchTo().window("");
+        driver.findElement(By.tagName("textarea")).sendKeys("e agora?");
 
 
     }
